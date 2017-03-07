82ac857e86a73e1cb6df82db5992e13d07f23802
experionarjun <arjun.sreedhar@experionglobal.com>
2017-03-07 12:15:58 +0530

    todo create

--- SOURCE/src/app/admin/admin-view-invoice/admin-view-invoice.component.ts
+++ SOURCE/src/app/admin/admin-view-invoice/admin-view-invoice.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {AdminService} from '../admin-services';
-import {Router} from '@angular/router'
-import {reset} from '../../login/login.component';
+import { AdminService } from '../admin-services';
+import { Router } from '@angular/router'
+import { reset } from '../../login/login.component';
 
 import * as jsPDF from 'jspdf';
 
@@ -11,50 +11,50 @@ import * as jsPDF from 'jspdf';
   selector: 'app-admin-view-invoice',
   templateUrl: './admin-view-invoice.component.html',
   styleUrls: ['./admin-view-invoice.component.css'],
-  providers : [AdminService]
+  providers: [AdminService]
 })
 export class AdminViewInvoiceComponent implements OnInit {
 
-  invoiceList:InvoiceList[];
-  invoiceDetails:InvoiceDetails;
-  test:itemList[];
-  saveStatusButton : boolean;
-  newStatus : string;
+  invoiceList: InvoiceList[];
+  invoiceDetails: InvoiceDetails;
+  test: itemList[];
+  saveStatusButton: boolean;
+  newStatus: string;
   detailOf: number;
-  statusChangeALert:reset;
+  statusChangeALert: reset;
 
-  totalItems:number ;
-  currentPage:number;
-  offset:number;
+  totalItems: number;
+  currentPage: number;
+  offset: number;
 
 
-  constructor(private AdminService: AdminService, private router : Router) {
-     this.currentPage = 1;
-     this.offset = 0;
+  constructor(private AdminService: AdminService, private router: Router) {
+    this.currentPage = 1;
+    this.offset = 0;
 
-     this.AdminService.getInvoiceCount().subscribe( res =>{
-       this.totalItems = res[0].count;
-     })
-
-      this.AdminService.getInvoiceList(this.offset).subscribe(res => {
-      this.invoiceList = res; 
+    this.AdminService.getInvoiceCount().subscribe(res => {
+      this.totalItems = res[0].count;
     })
 
-     this.statusChangeALert={
-        Fail:false,
-        Success:false,
-        Hidden:false
-      }
+    this.AdminService.getInvoiceList(this.offset).subscribe(res => {
+      this.invoiceList = res;
+    })
+
+    this.statusChangeALert = {
+      Fail: false,
+      Success: false,
+      Hidden: false
+    }
   }
 
 
   ngOnInit() {
-   
+
     this.saveStatusButton = false;
   }
 
 
- pageChanged(event: any): void {
+  pageChanged(event: any): void {
     this.offset = (event.page - 1) * 10;
     this.AdminService.getInvoiceList(this.offset).subscribe(res => {
       this.invoiceList = res;
@@ -62,17 +62,17 @@ export class AdminViewInvoiceComponent implements OnInit {
   }
 
 
-  showDetails(InvID,i){
-      this.statusChangeALert={
-        Success:false,
-        Fail:false,
-        Hidden:false
-      }
-      this.detailOf = i;
-      this.AdminService.getInvoiceDetails(InvID).subscribe( res => {
-      if(res.length!=0){
-        let dummyList =[]; 
-        this.invoiceDetails= {
+  showDetails(InvID, i) {
+    this.statusChangeALert = {
+      Success: false,
+      Fail: false,
+      Hidden: false
+    }
+    this.detailOf = i;
+    this.AdminService.getInvoiceDetails(InvID).subscribe(res => {
+      if (res.length != 0) {
+        let dummyList = [];
+        this.invoiceDetails = {
           Details: {
             InvoiceID: res[0].InvoiceID,
             UserID: res[0].UserID,
@@ -82,109 +82,109 @@ export class AdminViewInvoiceComponent implements OnInit {
             total: res[0].total
           },
 
-          Address:res[0].address,
-          dueDate:res[0].dueDate,
+          Address: res[0].address,
+          dueDate: res[0].dueDate,
           invoice_no: res[0].invoice_no,
 
-          List : []
+          List: []
         }
 
-        res.forEach(function(element){
+        res.forEach(function (element) {
           let list: itemList;
           list = {
-          item: element.item,
-          desc:element.desc,
-          unitp: element.unitp,
-          qty: element.qty
+            item: element.item,
+            desc: element.desc,
+            unitp: element.unitp,
+            qty: element.qty
           }
           dummyList.push(list);
-      })
-      this.invoiceDetails.List = dummyList;
-      this.invoiceDetails.Address = this.invoiceDetails.Address.split(',').join(',<br>')
-       }else{
-         alert("Something happened. Please try again");
-         this.router.navigate(['/admin']);
-       }
-      
-     })
+        })
+        this.invoiceDetails.List = dummyList;
+        this.invoiceDetails.Address = this.invoiceDetails.Address.split(',').join(',<br>')
+      } else {
+        alert("Something happened. Please try again");
+        this.router.navigate(['/admin']);
+      }
+
+    })
   }
 
-  saveNewSatus(value:string){
-    if(confirm("Do you want to update status?")){
+  saveNewSatus(value: string) {
+    if (confirm("Do you want to update status?")) {
       this.saveStatusButton = false;
-      this.AdminService.changeStatus(this.invoiceDetails.Details.InvoiceID,value).subscribe( res => {
-        if(res == 'updated'){
+      this.AdminService.changeStatus(this.invoiceDetails.Details.InvoiceID, value).subscribe(res => {
+        if (res == 'updated') {
           this.statusChangeALert.Success = true;
           this.statusChangeALert.Fail = false;
           this.invoiceDetails.Details.status = value;
           (<HTMLInputElement>document.getElementById('saveStatusSelect')).value = value;
-          this.invoiceList[this.detailOf].status = value; 
-        }else{
+          this.invoiceList[this.detailOf].status = value;
+        } else {
           this.statusChangeALert.Success = false;
           this.statusChangeALert.Fail = true;
           (<HTMLInputElement>document.getElementById('saveStatusSelect')).value = this.invoiceDetails.Details.status;
         }
       })
 
-    }else{
+    } else {
       this.saveStatusButton = false;
-      (<HTMLInputElement>document.getElementById('saveStatusSelect')).value = this.invoiceDetails.Details.status;  
+      (<HTMLInputElement>document.getElementById('saveStatusSelect')).value = this.invoiceDetails.Details.status;
     }
   }
 
-  savePDF(e:HTMLElement){
+  savePDF(e: HTMLElement) {
     let doc = new jsPDF();
     let specialElementHandlers = {
-        '#editor': function(element, renderer) {
-            return true;
-        }
+      '#editor': function (element, renderer) {
+        return true;
+      }
     };
-    doc.fromHTML(e,10, 10, {
-        'width': 180,
-        'elementHandlers': specialElementHandlers
+    doc.fromHTML(e, 10, 10, {
+      'width': 180,
+      'elementHandlers': specialElementHandlers
 
     });
     doc.save(this.invoiceDetails.invoice_no + ".pdf");
   }
 
-   printPDF(e:HTMLElement){
+  printPDF(e: HTMLElement) {
     let doc = new jsPDF();
     let specialElementHandlers = {
-        '#editor': function(element, renderer) {
-            return true;
-        }
+      '#editor': function (element, renderer) {
+        return true;
+      }
     };
-    doc.fromHTML(e,10, 10, {
-        'width': 180,
-        'elementHandlers': specialElementHandlers
+    doc.fromHTML(e, 10, 10, {
+      'width': 180,
+      'elementHandlers': specialElementHandlers
 
     });
-     doc.output("dataurlnewwindow");
+    doc.output("dataurlnewwindow");
   }
- 
+
 }
 
-interface InvoiceList{
-  InvoiceID:number;
-  UserID:string;
-  currency:string;
-  date_of_issue:Date;
-  status:string;
-  total:number;
+interface InvoiceList {
+  InvoiceID: number;
+  UserID: string;
+  currency: string;
+  date_of_issue: Date;
+  status: string;
+  total: number;
 }
 
-interface InvoiceDetails{
-  Details:InvoiceList;
+interface InvoiceDetails {
+  Details: InvoiceList;
   Address: string;
-  dueDate:Date;
-  invoice_no:string;
-  List:itemList[];
+  dueDate: Date;
+  invoice_no: string;
+  List: itemList[];
 }
 
-interface itemList{
-  item:string;
-  desc:string;
-  unitp:number;
-  qty:number;
+interface itemList {
+  item: string;
+  desc: string;
+  unitp: number;
+  qty: number;
 }
 
